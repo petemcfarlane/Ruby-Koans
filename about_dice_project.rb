078fc48aa019df1eca345d08902885071d70302a
Pete McFarlane <petemcfarlane@sontia.com>
2013-06-29 12:11:59 -0400

    up to 214/280

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -5,6 +5,15 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # class DiceSet
 #   code ...
 # end
+class DiceSet
+  attr_reader :values
+  def roll(amount)
+    @values = []
+    amount.downto(1) do
+      @values.push(rand(6))
+    end
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
